3a50782df5de34054ddef968f8a2313a8c8f5ca8
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-04-15 12:02:18 +0200

    add user not found exception

---
+++ src/main/java/io/github/teardrop13/authentication/exceptions/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package io.github.teardrop13.authentication.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(
+        value = HttpStatus.NOT_FOUND,
+        reason = "Requested user does not exist"
+)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
