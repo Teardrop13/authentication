bf6b9da0a6e61ec0766015e5dd9c5616cffdb215
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-04-15 21:24:16 +0200

    allow running an action after user creation

--- src/main/java/pl/teardrop/authentication/user/UserService.java
+++ src/main/java/pl/teardrop/authentication/user/UserService.java
@@ -7,61 +7,66 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
 public class UserService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Lazy
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Lazy
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    public boolean checkIfUserExists(@NonNull String username, @NonNull String email) {
-        try {
-            loadUserByUsername(username);
-            return true;
-        } catch (UsernameNotFoundException e) {
-            log.info("User not found for username: " + username);
-        }
+	public boolean checkIfUserExists(@NonNull String username, @NonNull String email) {
+		try {
+			loadUserByUsername(username);
+			return true;
+		} catch (UsernameNotFoundException e) {
+			log.info("User not found for username: " + username);
+		}
 
-        try {
-            loadUserByEmail(email);
-            return true;
-        } catch (UsernameNotFoundException e) {
-            log.info("User not found for email: " + email);
-        }
+		try {
+			loadUserByEmail(email);
+			return true;
+		} catch (UsernameNotFoundException e) {
+			log.info("User not found for email: " + email);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public User loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findUsersByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Failed to find user with username: " + username));
-    }
+	@Override
+	public User loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findUsersByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Failed to find user with username: " + username));
+	}
 
+	public User loadUserByEmail(String email) throws UsernameNotFoundException {
+		return userRepository.findUsersByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("Failed to find user with email: " + email));
+	}
 
-    public User loadUserByEmail(String email) throws UsernameNotFoundException {
-        return userRepository.findUsersByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Failed to find user with email: " + email));
-    }
+	public User create(String username, String password, String email) {
+		String encodedPassword = passwordEncoder.encode(password);
 
-    public User create(String username, String password, String email) {
-        String encodedPassword = passwordEncoder.encode(password);
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(encodedPassword);
+		user.setEmail(email);
 
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(encodedPassword);
-        user.setEmail(email);
+		User userAdded = save(user);
 
-        return save(user);
-    }
+		runAfterUserCreation(userAdded);
 
-    public User save(User user) {
-        return userRepository.save(user);
-    }
+		return userAdded;
+	}
+
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+
+	protected void runAfterUserCreation(User user) {
+
+	}
 }
