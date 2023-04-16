e98c68911de23c161263e3d921791f8e38fd7351
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-04-16 10:51:11 +0200

    formatting

--- src/main/java/pl/teardrop/authentication/configuration/WebSecurityConfig.java
+++ src/main/java/pl/teardrop/authentication/configuration/WebSecurityConfig.java
@@ -17,53 +17,47 @@ import pl.teardrop.authentication.user.UserService;
 
 import javax.servlet.http.HttpServletResponse;
 
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    private final SessionFilter sessionFilter;
+	private final SessionFilter sessionFilter;
 
-    @Lazy
-    private final PasswordEncoder passwordEncoder;
+	@Lazy
+	private final PasswordEncoder passwordEncoder;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+	}
 
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/api/register");
-//    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.cors().and()
+				.csrf().disable()//todo włączyć
+				.exceptionHandling().authenticationEntryPoint(
+						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage())
+				).and()
+				.addFilterBefore(
+						sessionFilter,
+						UsernamePasswordAuthenticationFilter.class
+				)
+				.authorizeRequests().antMatchers("/api/auth/login", "/api/auth/register").permitAll()
+				.and().authorizeRequests().anyRequest().authenticated();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and()
-                .csrf().disable()//todo włączyć
-                .exceptionHandling().authenticationEntryPoint(
-                        (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage())
-                ).and()
-                .addFilterBefore(
-                        sessionFilter,
-                        UsernamePasswordAuthenticationFilter.class
-                )
-                .authorizeRequests().antMatchers("/api/auth/login", "/api/auth/register").permitAll()
-                .and().authorizeRequests().anyRequest().authenticated();
-    }
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
