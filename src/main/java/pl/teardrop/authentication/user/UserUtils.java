8c6932306e653a54400e84730128521a99602dfe
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-08-27 14:26:45 +0200

    Make UserUtils return User instead of Optional

--- src/main/java/pl/teardrop/authentication/user/UserUtils.java
+++ src/main/java/pl/teardrop/authentication/user/UserUtils.java
@@ -5,19 +5,22 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.Optional;
+import pl.teardrop.authentication.exceptions.UserNotFoundException;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class UserUtils {
 
-    public static Optional<User> currentUser() {
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        Authentication authentication = securityContext.getAuthentication();
-        if (authentication != null) {
-            Object principal = authentication.getPrincipal();
-            return Optional.ofNullable(principal instanceof User user ? user : null);
-        }
-        return Optional.empty();
-    }
+	public static User currentUser() {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+
+		if (authentication == null
+			|| authentication.getPrincipal() == null
+			|| !(authentication.getPrincipal() instanceof User)) {
+			throw new UserNotFoundException("User not found.");
+		}
+
+		Object principal = authentication.getPrincipal();
+		return (User) principal;
+	}
 }
