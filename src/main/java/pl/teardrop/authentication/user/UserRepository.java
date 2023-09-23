9e9fa9924d7fe5f75d5013ba7b9db71f9fc34fa7
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-09-23 10:43:45 +0200

    Simplify UserRepository

--- src/main/java/pl/teardrop/authentication/user/UserRepository.java
+++ src/main/java/pl/teardrop/authentication/user/UserRepository.java
@@ -1,24 +1,15 @@
 package pl.teardrop.authentication.user;
 
-import org.springframework.data.repository.Repository;
-import org.springframework.stereotype.Component;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
-@Component
-public interface UserRepository extends Repository<User, Long> {
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
 
 	Optional<User> findUsersByUsername(String username);
 
 	Optional<User> findUsersByEmail(String email);
-
-	Optional<User> findById(Long id);
-
-	List<User> findAll();
-
-	User save(User user);
-
-	void deleteById(Long id);
 
 }
